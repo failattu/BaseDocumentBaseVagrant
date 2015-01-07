dcbb5f3a5f445547e96ed350761aaa5fcec06770
Failattu <kalle.sukeltaja@gmail.com>
2015-01-07 20:55:25 +0200

    CouchDB setup

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update -y
-sudo apt-get install -y mongodb-org git
-sudo service mongod start
-sudo apt-get -y update
+sudo apt-get install couchdb -y
 sudo apt-get -y install nodejs node
 sudo apt-get -y install npm
 echo 'var http = require("http");' > index.js
