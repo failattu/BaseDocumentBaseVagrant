e8f93cc807efc35cd05c040acf0b91fc5700d235
Failattu <kalle.sukeltaja@gmail.com>
2015-01-07 20:44:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+These are my setups I will try to use branches with these boxes. 
+
+Master == Node.JS + MongoDB
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update -y
+sudo apt-get install -y mongodb-org git
+sudo service mongod start
+sudo apt-get -y update
+sudo apt-get -y install nodejs node
+sudo apt-get -y install npm
+echo 'var http = require("http");' > index.js
+echo 'http.createServer(function (req, res) {'>> index.js
+echo '  res.writeHead(200, {"Content-Type": "text/plain"});'>> index.js
+echo '  res.end("Hello World\n");'>> index.js
+echo '}).listen(8080);'>> index.js
+echo 'console.log("Server running at http://:8080/");'>> index.js
+nodejs index.js
